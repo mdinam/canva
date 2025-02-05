803b0de8c5fbbc6f06cb45db6b81957277d7919b
unknown <muhammadinamdar1982@gmail.com>
2025-02-05 19:09:27 +0530

    canva home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,44 +9,7 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-<<<<<<< HEAD
-  return (
-<<<<<<< HEAD
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      <h1>I LOVE FARAH SULTANA
-      </h1>
-    
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-=======
-    <div>
-      <h1>I love you Farah Sultana</h1>
-    </div>
-  );
->>>>>>> bb4f1fc (first commit)
-=======
   return <RouterProvider router={router} />;
->>>>>>> 3045874 (app.jsx changed)
 }
 
 export default App;
